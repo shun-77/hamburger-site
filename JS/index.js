e872bbd085d78dc25393a33610230524a329672c
shun-77 <shun7715k@icloud.com>
2024-01-09 16:13:15 +0900

    フロントページ（sp）コーディング完了①

---
+++ JS/index.js
@@ -0,0 +1,19 @@
+document.querySelector('.js-header__menu-btn--open').addEventListener(
+    'click',
+    ()=>{
+    document.querySelector('.js-header__menu-btn--open').classList.add('inactive');
+    document.querySelector('.js-header__menu-btn--close').classList.add('active');
+    document.querySelector('.js-menu').classList.add('active');
+    document.querySelector('.js-cover').classList.add('active');
+    }
+);
+
+document.querySelector('.js-header__menu-btn--close').addEventListener(
+    'click',
+    ()=>{
+    document.querySelector('.js-header__menu-btn--open').classList.remove('inactive');
+    document.querySelector('.js-header__menu-btn--close').classList.remove('active');
+    document.querySelector('.js-menu').classList.remove('active');
+    document.querySelector('.js-cover').classList.remove('active');
+    }
+);
