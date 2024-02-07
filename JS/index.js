bfc1028b12068539a1b7808007ca17ab9bc2a7d3
shun-77 <shun7715k@icloud.com>
2024-02-07 19:44:29 +0900

    メニューのスクロールに関しての修正

--- JS/index.js
+++ JS/index.js
@@ -5,6 +5,7 @@ document.querySelector('.js-header__menu-btn--open').addEventListener(
     document.querySelector('.js-header__menu-btn--close').classList.add('active');
     document.querySelector('.js-menu').classList.add('active');
     document.querySelector('.js-cover').classList.add('active');
+    document.querySelector('html').classList.add('scroll-prevent');
     }
 );
 
@@ -15,29 +16,6 @@ document.querySelector('.js-header__menu-btn--close').addEventListener(
     document.querySelector('.js-header__menu-btn--close').classList.remove('active');
     document.querySelector('.js-menu').classList.remove('active');
     document.querySelector('.js-cover').classList.remove('active');
+    document.querySelector('html').classList.remove('scroll-prevent');
     }
 );
-
-// 高さを揃える要素
-const target = document.querySelectorAll('.height');
- 
-// 空の配列を指定。
-const heightlist = [];
- 
-// 要素の高さを全て配列に格納
-target.forEach(element => {
-  const height = element.clientHeight;
-  heightlist.push(height);
-});
- 
-//一番高さがある要素のheightを取得。
-const maxHeight = Math.max.apply(null, heightlist);
- 
-
-if (window.matchMedia('(max-width: 1023.9999999px)').matches) {
-// 要素にstyle属性を付与。一番高さのある要素のheightを揃える要素全てに指定。
-    target.forEach(element => {
-        element.style.height = maxHeight + 'px';
-    }); 
-};
-
